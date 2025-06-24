13cd01b0dbc0665fd8f27897d7df5310bb999357
Anikethgowda <anikethgowda08@gmail.com>
2025-06-24 20:49:20 +0530

    Add files via upload

---
+++ Sampleoutput.java
@@ -0,0 +1,6 @@
+Enter password length: 12
+Include uppercase letters? (true/false): true
+Include lowercase letters? (true/false): true
+Include numbers? (true/false): true
+Include special characters? (true/false): true
+Generated Password: A9b#qT@8kX2!
